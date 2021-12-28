43e58b06d613f0116c3656c7b48d99905a89aa90
Francesco <francescosalvatori99@gmail.com>
2021-12-28 18:22:48 +0100

    Aggiunti timestamp ad ogni spostamento nelle cartelle: Risolvere bug di attesa download file prima del trasferimento

--- dldeclutter.py
+++ dldeclutter.py
@@ -1,8 +1,8 @@
 import os
 import time
-import json
-import shutil
 import collections
+
+from datetime import datetime
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
@@ -13,9 +13,12 @@ IMAGE_FILES = ['png', 'jpeg', 'jpg', 'gif', 'svg', 'tiff', 'tif', 'psd', 'bmp']
 DOCS_FILES  = ['pdf', 'docs', 'latex', 'csv', 'xls', 'doc', 'xlsx', 'odt', 'tex']
 COMPR_FILES = ['zip', '7z', 'z', 'pkg','rar', 'tar']
 
-DEST_DIRS = ['Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other', 'Random']
+DEST_DIRS = ['Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other', 'Random'] 
 
 def move_files() :
+
+    TIMESTAMP = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
     for d in DEST_DIRS:
         dir_path = os.path.join(BASE_PATH, d)
         if not os.path.isdir(dir_path):
@@ -28,38 +31,52 @@ def move_files() :
         if filename[0] != '.' : 
             file_ext = filename.split('.')[-1]
             files_mapping[file_ext].append(filename)
+            
 
 
     for f_ext, f_list in files_mapping.items() :
         if f_ext in AUDIO_FILES :
             for file in f_list :
+                name = file.split('.')[0]
+                name = str(name) + "-" + TIMESTAMP + "." + f_ext
+
                 os.rename(
                     os.path.join(DOWNLOAD_PATH, file), 
-                    os.path.join(BASE_PATH, 'Music', file))
+                    os.path.join(BASE_PATH, 'Music', name))
 
         elif f_ext in DOCS_FILES or f_ext in COMPR_FILES:
             for file in f_list :
+                name = file.split('.')[0]
+                name = str(name) + "-" + TIMESTAMP + "." + f_ext
                 os.rename(
                     os.path.join(DOWNLOAD_PATH, file), 
-                    os.path.join(BASE_PATH, 'Documents', file))
+                    os.path.join(BASE_PATH, 'Documents',  name))
         
         elif f_ext in VIDEO_FILES :
             for file in f_list :
+                name = file.split('.')[0]
+                name = str(name) + "-" + TIMESTAMP + "." + f_ext
+
                 os.rename(
                     os.path.join(DOWNLOAD_PATH, file), 
-                    os.path.join(BASE_PATH, 'Movies', file))
+                    os.path.join(BASE_PATH, 'Movies', name))
 
         elif f_ext in IMAGE_FILES :
             for file in f_list :
+                name = file.split('.')[0]
+                name = str(name) + "-" + TIMESTAMP + "." + f_ext
+
                 os.rename(
                     os.path.join(DOWNLOAD_PATH, file), 
-                    os.path.join(BASE_PATH, 'Pictures', file))
+                    os.path.join(BASE_PATH, 'Pictures', name))
         
         else:
             for file in f_list :
+                name = file.split('.')[0]
+                name = str(name) + "-" + TIMESTAMP + "." + f_ext
                 os.rename(
                     os.path.join(DOWNLOAD_PATH, file), 
-                    os.path.join(BASE_PATH, 'Other', file))
+                    os.path.join(BASE_PATH, 'Other', name))
 
 # Path home 
 BASE_PATH = os.path.expanduser('~')
