81c8a2cccfab1bcbe8ad9c4ec1e4bf03bd2c48b0
Francesco <francescosalvatori99@gmail.com>
2021-12-28 18:54:40 +0100

    Added comments

--- dldeclutter.py
+++ dldeclutter.py
@@ -6,40 +6,44 @@ from datetime import datetime
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
-# Estensioni da riconoscere
+# Extentions to be looked up:
 AUDIO_FILES = ['mp3', 'wav', 'raw', 'wma', 'aiff', 'mp2']
 VIDEO_FILES = ['webm', 'mp4', 'mpg', 'mpeg', 'avi', 'mov', 'flv', 'mkv', 'h264']
 IMAGE_FILES = ['png', 'jpeg', 'jpg', 'gif', 'svg', 'tiff', 'tif', 'psd', 'bmp']
 DOCS_FILES  = ['pdf', 'docs', 'latex', 'csv', 'xls', 'doc', 'xlsx', 'odt', 'tex']
 COMPR_FILES = ['zip', '7z', 'z', 'pkg','rar', 'tar']
 
+# Directiories we want to create (if non-existant) 
 DEST_DIRS = ['Music', 'Movies', 'Pictures', 'Documents', 'Applications', 'Other', 'Random'] 
 
+# Rename file, attaching a timestamp. 
 def rename_file(ext_type, file_name, destination_folder) :
     TIMESTAMP = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     name = file_name.split('.')[0]
     name = str(name) + "-" + TIMESTAMP + "." + ext_type
 
+    # Rename on filesystem
     os.rename(os.path.join(DOWNLOAD_PATH, file_name), os.path.join(BASE_PATH, destination_folder, name)) 
 
-def move_files() :
-
-
+def create_directories() : 
     for d in DEST_DIRS:
         dir_path = os.path.join(BASE_PATH, d)
         if not os.path.isdir(dir_path):
             os.mkdir(dir_path)
 
-    files_mapping = collections.defaultdict(list)
-    files_list = os.listdir(DOWNLOAD_PATH)
 
-    for filename in files_list:
-        if filename[0] != '.' : 
-            file_ext = filename.split('.')[-1]
-            files_mapping[file_ext].append(filename)
+def move_files() :
+
+    files_mapping = collections.defaultdict(list) # Gets a dictionary
+    
+    files_list = os.listdir(DOWNLOAD_PATH) # Gets all files in downloads folder
+
+    for filename in files_list: 
+        if filename[0] != '.' : # Ignore '.' folders
+            file_ext = filename.split('.')[-1]  # Get extentions to create keys for the dictionary
+            files_mapping[file_ext].append(filename) # Populate the dictionary
             
-
-
+    # For each extentions, renames the files and moves it into the correct directory.
     for f_ext, f_list in files_mapping.items() :
         if f_ext in AUDIO_FILES :
             for file in f_list :
@@ -56,31 +60,37 @@ def move_files() :
         elif f_ext in IMAGE_FILES :
             for file in f_list :
                 rename_file(f_ext, file, 'Pictures')
-        
+                
         else:
             for file in f_list :
                 rename_file(f_ext, file, 'Other')
 
-# Path home 
+# Path to home
 BASE_PATH = os.path.expanduser('~')
 
-# Path cartella da osservare per i cambiamenti (nel nostro caso la cartella downlads)
+# Path to download folder. 
 DOWNLOAD_PATH = os.path.join(BASE_PATH, 'Downloads')
 
-# Vogliamo controllare i cambiamenti sulla cartella osservata. 
 class FileHandler(FileSystemEventHandler):
+    # When change occurs
     def on_modified(self, event):
-        time.sleep(2)
-        move_files()
+        time.sleep(2)   # Give it some time to transfer the file [TODO: Breakable]
+        move_files()    # Move it
 
-# Istanza della classe che si occupa di controllare le modifiche.
+# File system handler.
 event_handler = FileHandler()
+# File system observer
 observer = Observer()
 
+# Create the directories, if needed.
+create_directories()
+
+# Looks up for changes in DOWNLOAD_PATH
 observer.schedule(event_handler, DOWNLOAD_PATH, recursive=True)
 observer.start()
 
 try: 
+    # Always sleep until killed:
     while True:
         time.sleep(10)
 except KeyboardInterrupt:
